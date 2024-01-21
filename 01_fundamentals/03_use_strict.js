82c38929499bf2ff00e2f549ab7a021bf92eca2d
Simon K <jackofspaces@gmail.com>
2024-01-21 12:42:55 +0000

    add note about implicit strictness

--- 01_fundamentals/03_use_strict.js
+++ 01_fundamentals/03_use_strict.js
@@ -2,7 +2,10 @@
 can enable more modern features of the javascript language at 
 breaking older backwards compatibility.  use strict can be placed
 on a per function basis, however often it is best used at the
-top of the file like below to have full compatibility throughout. */
+top of the file like below to have full compatibility throughout.
+
+use strict is also implicit, when dealing with things like modern classes
+and objects etc.*/
 
 // ... any code before the declaration would not have strict applied.
 
