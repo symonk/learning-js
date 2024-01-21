a3544a4c5d16e3f9d7c3cbc81577cf95ccd4e8eb
Simon K <jackofspaces@gmail.com>
2024-01-21 16:44:27 +0000

    adding data for `alerts`; `confirm` & `prompt`

--- 01_fundamentals/06_alert_prompt_confirm.js
+++ 01_fundamentals/06_alert_prompt_confirm.js
@@ -1,13 +1,12 @@
 //These following are browser specific.
 
 // Alert the user of some action;
-alert("This pops up an alert in the browser for the user to press OK on");
+alert('This pops up an alert in the browser for the user to press OK on');
 
 // Capture user input with a sensible default;
 // This result can be the Null type if cancel/esc is pressed;
-result = prompt("What is your name?", "George");
-
+result = prompt('What is your name?', 'George');
 
 // Confirm with a logical boolean, similar to result
 // OK = true; otherwise false;
-let wasTrue = confirm("is it true?");
+let wasTrue = confirm('is it true?');
