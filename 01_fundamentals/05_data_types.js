79e9680e83b052caa56545049341b56471a9f782
Simon K <jackofspaces@gmail.com>
2024-01-21 16:25:19 +0000

    `BigInt` and `Strings`

--- 01_fundamentals/05_data_types.js
+++ 01_fundamentals/05_data_types.js
@@ -29,11 +29,51 @@ function Numbers() {
     console.log(nan);
 }
 
+// The BigInt data type
+function BigIntegers() {
+    // The number type system can store numbers that fit in a 64 bit storage
+    // this equates to 2 to the power 53 minus 1
+
+    // two number types exceeding the range
+    const maximum = Math.pow(2, 53) - 1;
+    let plusOne = maximum + 1;
+    let plusTwo = maximum + 2;
+    // They strictly equal the same, we fix this below
+    console.log(plusOne === plusTwo)
+
+    // Big integers have an 'n' suffix
+    const bigNumber = 9999999999999999999999999999999999999999999999n;
+    // `BigInt`
+    console.log(typeof bigNumber);
+}
+
+// The string data type
+function Strings() {
+    // Single and Double quotes are largely the same
+    // backticks offer additional functionality.
+    let doubleQuoteString = "doubleQuoted";
+    let singleQuoteString = 'singleQuoted';
+    let backTicksString = `backTickString`;
+    console.log(doubleQuoteString);
+    console.log(singleQuoteString);
+    console.log(backTicksString);
+
+    // Backticks allow expression evaluation and interpolation into the string:
+    const name = "George";
+    let template = `Hello ${name}`;
+    // Prints 'Hello George'.
+    console.log(template);
+
+    // Javascript does NOT have a char type for a single code point!
+}
+
 
 
 function Main() {
-    Numbers()
-
+    Numbers();
+    BigIntegers();
+    Strings();
+    Booleans();
 }
 
 
